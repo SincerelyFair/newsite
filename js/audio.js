32777d6633877f5c84cf738c9661aa41c425dabe
xml <82622753+SincerelyFair@users.noreply.github.com>
2024-06-11 19:53:59 +0100

    audio 9

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,4 @@
-const audioFiles = ['audio/1.mp3', 'audio/2.mp3', 'audio/3.mp3', 'audio/4.mp3', 'audio/5.mp3', 'audio/6.mp3', 'audio/7.mp3', 'audio/8.mp3']; // Add more file names as needed
+const audioFiles = ['audio/1.mp3', 'audio/2.mp3', 'audio/3.mp3', 'audio/4.mp3', 'audio/5.mp3', 'audio/6.mp3', 'audio/7.mp3', 'audio/8.mp3', 'audio/9.mp3'];
 
 function play() {
     // Select a random audio file from the array
